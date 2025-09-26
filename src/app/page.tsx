2d32eaa95ccb98d29ac4fce8bb8d7d1f333f287b
ItikawaBruno <brunoitikawa@gmail.com>
2025-09-26 08:58:11 -0300

    commit em casa trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { prisma } from "@/lib/db";
+import { useTRPC } from "@/trpc/client";
 
 export default async function Page(){
+
+  const trpc = useTRPC()
+  trpc.hello.queryOptions({text:"Hello!"})
 
   const posts = await prisma.post.findMany();
 
